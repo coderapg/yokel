ff187277422e25a4eb7d42e3296f9b1619f52681
李永峰 <1184156787@qq.com>
2022-12-05 00:16:50 +0800

    封装首页选项卡模块

---
+++ src/components/content/Tabs/Tabs.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tabs">
+    <div
+      class="tab-item"
+      v-for="(item, index) in controlList"
+      :key="index"
+      :class="currIndex === index ? 'active' : ''"
+      @click="handleTabItemClick(index)">
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabs',
+  props: {
+    controlList: {
+      type: Array,
+      default () {
+        return []
+      },
+      require: true
+    }
+  },
+  data () {
+    return {
+      currIndex: 0
+    }
+  },
+  methods: {
+    handleTabItemClick (index) {
+      this.currIndex = index
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .tabs {
+    display: flex;
+    .tab-item {
+      flex: 1;
+      text-align: center;
+      line-height: 44px;
+      span {
+        padding-bottom: 6px;
+        border-bottom: 2px solid transparent;
+      }
+    }
+    .tab-item.active {
+      span {
+        color: #ff6700;
+        border-bottom-color: #ff6700;
+      }
+    }
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,6 +6,29 @@
     <home-swiper :bannerList="bannerList"/>
     <home-recommend :recommendList="recommendList"/>
     <home-popular />
+    <tabs :controlList="['流行', '新款', '精选']"/>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+    </ul>
   </div>
 </template>
 
@@ -14,6 +37,7 @@ import NavBar from 'components/common/NavBar/NavBar'
 import HomeSwiper from './components/HomeSwiper'
 import HomeRecommend from './components/HomeRecommend'
 import HomePopular from './components/HomePopular'
+import Tabs from 'components/content/Tabs/Tabs'
 import { getHomeMultidata } from 'https/home'
 
 export default {
@@ -22,12 +46,27 @@ export default {
     NavBar,
     HomeSwiper,
     HomeRecommend,
-    HomePopular
+    HomePopular,
+    Tabs
   },
   data () {
     return {
       bannerList: [],
-      recommendList: []
+      recommendList: [],
+      controlList: [
+        {
+          id: 0,
+          text: '今年'
+        },
+        {
+          id: 1,
+          text: '明年'
+        },
+        {
+          id: 2,
+          text: '后年'
+        }
+      ]
     }
   },
   created () {
